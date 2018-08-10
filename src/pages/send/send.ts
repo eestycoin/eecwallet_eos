6c071e9d0c21b8868227cb88765727f226b86d83
Alex Babichev <alex.babichev@me.com>
2018-08-10 08:36:40 +0200

    min max

--- src/pages/send/send.ts
+++ src/pages/send/send.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { EthProvider } from '../../providers/eth/eth';
@@ -15,7 +15,8 @@ import { environment } from '../../app/environment';
 export class SendPage {
 
   addressTo: string = environment.eth.wallet;
-  amount: number = 0.000001;
+  amount: number = 1;
+  max = 1;
 
   constructor(
     public navCtrl: NavController,
@@ -24,11 +25,23 @@ export class SendPage {
     private db: FirebaseProvider
   ) { }
 
+  @HostListener('input', ['$event'])
+    onInput(event: Event) {
+      const el = <HTMLInputElement>event.srcElement;
+      el.value = el.value.replace(/[^0-9]/g, '');
+    }
+
   onSubmit() {
     if (!this.amount || !this.addressTo)
       return;
 
     this.navCtrl.push('ConfirmPage', { func: this.onTransfer.bind(this) })
+  }
+
+  onChange(e: number) {
+    setTimeout(() => {
+      this.amount = (this.amount >= this.max) ? this.max : this.amount;
+    });
   }
 
   async onTransfer() {
