fa56dab87ac72af92b8c8648f5f65f9053629c9b
Alex Babichev <alex.babichev@me.com>
2018-08-13 08:04:39 +0200

    firestore

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -33,12 +33,12 @@ export const environment = {
     }
   },
   firebase: {
-    apiKey: 'AIzaSyBMguwZpwMNKw_Q8-WrUJ5rbNi7hbZKcxo',
-    authDomain: 'veiko-77bd8.firebaseapp.com',
-    databaseURL: 'https://veiko-77bd8.firebaseio.com',
-    projectId: 'veiko-77bd8',
-    storageBucket: 'veiko-77bd8.appspot.com',
-    messagingSenderId: '264863290527'
+    apiKey: 'AIzaSyBIbwROqWi6iwhjavQolloL6wZam1bkFt0',
+    authDomain: 'eesty-coin.firebaseapp.com',
+    databaseURL: 'https://eesty-coin.firebaseio.com',
+    projectId: 'eesty-coin',
+    storageBucket: 'eesty-coin.appspot.com',
+    messagingSenderId: '871257659837'
   }
 }
 
