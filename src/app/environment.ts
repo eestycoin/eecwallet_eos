5f924346838225ef0e9105680356b52a9554eab6
Alex Babichev <alex.babichev@me.com>
2018-08-19 10:21:02 +0300

    prod env

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -10,18 +10,18 @@ export const environment = {
     currencies: ['BTC', 'ETH', 'BCH', 'LTC', 'ZEC']
   },
   bitgo: {
-    accessToken: 'v2x8473d567ae7b4bff3895cdf4dca67a85c53f0aabad726d6061794505d32b16a3',
+    accessToken: 'v2x1e100e38e323b59c426a6d3372b14e4e00ad9d5e447b147c0ad087e7dfb8283a',
     apiUrl: 'https://proxy2bitgo.eestycoin.biz/api/v2',
     walletIndex: 0,
-    currencyPrefix: 't'
+    currencyPrefix: ''
   },
   eth: {
     apiUrl: 'https://proxy2infura.eestycoin.biz/v3/fac5373658944a8a860c901f79dfe34d',
-    contractAddr: '0x5D21c109a3A44466d0C79a8B7b8779d289B5Db8C',
+    contractAddr: '0x6fd1dada61f2d210a1e4e47b672f73bc851862f9',
     networkId: 3,
     interval: 1000,
     testPrivateKey: 'bd013827c4657f3d27522e266f783d87b545cc5bc4cbb12788f3cee88134c5a9',
-    wallet: '0x38bD7BaDAa300D8d40dca0BfbbCab1e0485dD123',
+    wallet: '0x724F8145B2a28E4935BCF09477eFD51Ce68B2cdD',
   },
   rates: {
     apiUrl: 'https://api.coinmarketcap.com/v2/ticker/',
