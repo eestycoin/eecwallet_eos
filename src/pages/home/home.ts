1c480b8d3e5237baa5ab00a41cb65779301b92d6
Kostiantyn Kliuiev <kkliuiev@vareger.com>
2019-04-24 08:13:53 +0300

    copy to clipboard added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,7 +17,19 @@ export class HomePage {
   itemSelected(item: string) {
     this.navCtrl.push(item);
   }
-
+  copyMessage(val: string){
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
   signOut() {
     this.eth.signOut();
     this.navCtrl.push('LoginPage');
