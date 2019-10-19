347fb481990cd69a6217dac03a23149aac4397ba
Alex Babichev <alex.babichev@me.com>
2019-10-19 14:27:23 +0200

    _

--- src/providers/qr-scaner/qr-scaner.ts
+++ src/providers/qr-scaner/qr-scaner.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 import QRReader from 'qrreader';
 
 // import QRReader from './temp';
@@ -9,8 +10,14 @@ export class QRScaner {
   private qrReader: QRReader; 
   private interval: 500;
 
-  constructor() {
-    this.qrReader = new QRReader();
+  constructor(private qrScanner: QRScanner) {
+    // this.qrReader = new QRReader();
+
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        console.log(status);
+      })
+      .catch((e: any) => console.log('Error is', e));
   }
 
   public startCapture(videoEl: HTMLElement): Promise<string> {
