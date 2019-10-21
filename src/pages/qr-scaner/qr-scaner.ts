ad352b0145aceae64d6867f66a9cd71e4499b5ca
Alex Babichev <alex.babichev@me.com>
2019-10-21 15:38:16 +0200

    fixed

--- src/pages/qr-scaner/qr-scaner.ts
+++ src/pages/qr-scaner/qr-scaner.ts
@@ -52,7 +52,7 @@ export class QrScanerModal {
   ionViewDidLoad() {
     window.disableFaio = true;
 
-    console.log(123, this.platform.is('cordova'), this.platform.is('ios'));
+    // console.log(123, this.platform.is('cordova'), this.platform.is('ios'));
 
     if (this.platform.is('cordova') && this.platform.is('ios')) {
       this.qrScanner.prepare()
@@ -60,7 +60,10 @@ export class QrScanerModal {
           console.log(status);
           this.startCaptureIos(status);
         })
-        .catch((e: any) => console.log('Error is', e));
+        .catch((e: any) => {
+          console.log('Error is', e);
+          this.viewCtrl.dismiss();
+        });
 
       return;
     }
@@ -84,7 +87,8 @@ export class QrScanerModal {
   startCaptureIos(status) {
     if (status.authorized) {
       // camera permission was granted
-
+      console.log("scanning")
+      window.document.querySelector('ion-app').classList.add('transparentBody')
 
       // start scanning
       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
@@ -96,13 +100,19 @@ export class QrScanerModal {
 
         this.qrScanner.hide(); // hide camera preview
         scanSub.unsubscribe(); // stop scanning
+
+        window.document.querySelector('ion-app').classList.remove('transparentBody')
       });
 
+      this.qrScanner.show();
+
     } else if (status.denied) {
+      console.log("Denied permission to access camera");
       // camera permission was permanently denied
       // you must use QRScanner.openSettings() method to guide the user to the settings page
       // then they can grant the permission from there
     } else {
+      console.log("Something else is happening with the camera");
       // permission was denied, but not permanently. You can ask for permission again at a later time.
     }
   }
